d506c0ac92e36a8a44bd5f2a997f9d66f58c7985
Jairo Sanchez <69741616+sanchezjairo@users.noreply.github.com>
2022-06-01 12:53:34 -0700

    Users can see all mock events

--- app.js
+++ app.js
@@ -14,7 +14,19 @@ app.set('view engine', 'handlebars');
 
 // Render the "home" layout for the main page and send the following msg
 app.get('/', (req, res) => {
-    res.render('home', { msg: 'Handlebars are Cool!' });
+    res.render('events-index', { events: events });
+})
+
+// OUR MOCK ARRAY OF PROJECTS
+var events = [
+    { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+    { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+    { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
+  ]
+  
+  // INDEX
+  app.get('/events', (req, res) => {
+    res.render('events-index', { events: events });
 })
 
 // Choose a port to listen on
