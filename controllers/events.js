685036c7d4db9c8065ffe7daca2bf5db429521ef
Jairo Sanchez <69741616+sanchezjairo@users.noreply.github.com>
2022-06-15 14:03:23 -0700

    added createdAt, footer, and bootstrap theme

--- controllers/events.js
+++ controllers/events.js
@@ -24,15 +24,15 @@ module.exports = function (app, models) {
 
 // SHOW
  app.get('/events/:id', (req, res) => {
-    // Search for the event by its id that was passed in via req.params
     models.Event.findByPk(req.params.id, { include: [{ model: models.Rsvp }] }).then(event => {
-      // If the id is for a valid event, show it
-      res.render('events-show', { event: event })
+        let createdAt = event.createdAt;
+        createdAt = moment(createdAt).format('MMMM Do YYYY, h:mm:ss a');
+        event.createdAtFormatted = createdAt;
+        res.render('events-show', { event: event });
     }).catch((err) => {
-      // if they id was for an event not in our db, log an error
-      console.log(err.message);
+        console.log(err.message);
     })
-  })
+});
 
 // EDIT
  app.get('/events/:id/edit', (req, res) => {
