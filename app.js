e3c5fc121b4b9d8fecd4a7b4be408c7cb100f01a
Jairo Sanchez <69741616+sanchezjairo@users.noreply.github.com>
2022-06-15 10:50:37 -0700

    Users can see single events

--- app.js
+++ app.js
@@ -41,7 +41,8 @@ var events = [
 // CREATE
 app.post('/events', (req, res) => {
   models.Event.create(req.body).then(event => {
-    res.redirect(`/`);
+    // Redirect to events/:id
+    res.redirect(`/events/${event.id}`)
   }).catch((err) => {
     console.log(err)
   });
@@ -50,6 +51,18 @@ app.post('/events', (req, res) => {
 // NEW
 app.get('/events/new', (req, res) => {
   res.render('events-new', {});
+})
+
+// SHOW
+app.get('/events/:id', (req, res) => {
+  // Search for the event by its id that was passed in via req.params
+  models.Event.findByPk(req.params.id).then((event) => {
+    // If the id is for a valid event, show it
+    res.render('events-show', { event: event })
+  }).catch((err) => {
+    // if they id was for an event not in our db, log an error
+    console.log(err.message);
+  })
 })
 
 // Choose a port to listen on
